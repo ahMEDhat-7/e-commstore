26e6c354f2c256918fa18b8802b14203c8e93259
ahmed medhat <am510836@gmail.com>
2025-09-15 20:54:48 +0300

    add: signup page & logic & error handling

--- frontend/src/common/types.ts
+++ frontend/src/common/types.ts
@@ -14,6 +14,7 @@ export interface SignupType {
   username: string;
   email: string;
   password: string;
+  confirmPassword: string;
 }
 
 // Login DTO
--- frontend/src/store/auth/thunk.ts
+++ frontend/src/store/auth/thunk.ts
@@ -2,16 +2,20 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { loginAPI, signupAPI } from "../../common/api/authApi";
 import type { LoginType, SignupType } from "../../common/types";
 import { toast } from "react-hot-toast";
+import type { AxiosError } from "axios";
 
 const loginThunk = createAsyncThunk(
   "auth/login",
   async (userData: LoginType, thunkAPI) => {
     try {
       const user = await loginAPI(userData);
+      toast.success(user.data.message);
       return user.data;
-    } catch (error) {
-      toast.error("Login Failed !");
-      return thunkAPI.rejectWithValue(error);
+    } catch (err) {
+      const error = err as AxiosError;
+
+      toast.error((error.response?.data as any).message);
+      return thunkAPI.rejectWithValue((error.response?.data as any).message);
     }
   }
 );
@@ -20,10 +24,18 @@ const signupThunk = createAsyncThunk(
   "auth/signup",
   async (userData: SignupType, thunkAPI) => {
     try {
+      if (userData.password !== userData.confirmPassword) {
+        toast.error("Passwords do not match");
+        return thunkAPI.rejectWithValue("Passwords do not match");
+      }
       const user = await signupAPI(userData);
+      toast.success(user.data.message);
       return user.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
+    } catch (err) {
+      const error = err as AxiosError;
+
+      toast.error((error.response?.data as any).message);
+      return thunkAPI.rejectWithValue((error.response?.data as any).message);
     }
   }
 );
